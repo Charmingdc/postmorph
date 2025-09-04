b832052069794b9ae6bae467e77aaf91870efa9e
Charmingdc <charmingdc002@gmail.com>
2025-09-04 14:11:58 +0100

    Fix lint bug

--- src/app/(userpages)/editor/lib/fetchUniqueDraft.ts
+++ src/app/(userpages)/editor/lib/fetchUniqueDraft.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
-import type { DraftType } from "@/lib/types";
+import type { DraftType } from "@/types/index";
 
 const fetchUniqueDraft = async (
   userId: string,
