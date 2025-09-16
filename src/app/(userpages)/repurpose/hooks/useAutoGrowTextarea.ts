82f22d283b95c3e391601fd30e139e7c5d895709
Charmingdc <charmingdc002@gmail.com>
2025-09-16 15:21:03 +0100

    Update useAutoGrowArea hook to handle unexpected cases like null ref and window resize

--- src/app/(userpages)/repurpose/hooks/useAutoGrowTextarea.ts
+++ src/app/(userpages)/repurpose/hooks/useAutoGrowTextarea.ts
@@ -1,17 +1,25 @@
 import { useEffect, RefObject } from "react";
 
 export default function useAutoGrowTextarea(
-  ref: RefObject<HTMLTextAreaElement>,
+  ref: RefObject<HTMLTextAreaElement | null>,
   value: string
 ) {
   useEffect(() => {
     const el = ref.current;
-    if (el) {
-      const maxHeight = 450;
+    if (!el) return;
 
+    const maxHeight = 450;
+
+    const resize = () => {
       el.style.height = "auto";
       const newHeight = Math.min(el.scrollHeight, maxHeight);
       el.style.height = `${newHeight}px`;
-    }
+    };
+    resize();
+
+    window.addEventListener("resize", resize);
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
   }, [ref, value]);
 }
