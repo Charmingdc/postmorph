f2cfb8f9109eb748c9f92ecbc8037205a22fd6f4
Charmingdc <charmingdc002@gmail.com>
2025-09-18 16:39:07 +0100

    Minor UI fix

--- src/app/(userpages)/editor/components/PromptPopover.tsx
+++ src/app/(userpages)/editor/components/PromptPopover.tsx
@@ -133,7 +133,7 @@ content: ${content}`);
               className="h-9 w-9 flex items-center justify-center p-1.5 border-l disabled:opacity-50"
             >
               {isPending ? (
-                <Spinner w="w-[.5rem]" h="h-[.5rem]" />
+                <Spinner w="w-[1rem]" h="h-[1rem]" />
               ) : (
                 <WandSparkles
                   size={18}
