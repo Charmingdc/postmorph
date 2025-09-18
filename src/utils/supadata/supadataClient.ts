fe01ac1e6d08d71a9b84683a72802551d3d9d4f0
Charmingdc <charmingdc002@gmail.com>
2025-09-18 16:10:59 +0100

    Minor Fix

--- src/utils/supadata/supadataClient.ts
+++ src/utils/supadata/supadataClient.ts
@@ -1,4 +1,5 @@
-// @ts-ignore
+// @ts-expect-error : Supadata types are missing or mismatched in the installed version.
+// Using the library as per official documentation; types will be updated when Supadata releases proper type definitions.
 import { Supadata } from "@supadata/js";
 
 export const supadata = new Supadata({
